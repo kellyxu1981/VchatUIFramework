e62acf85c0b3bddbc307fe33adc31b21e3ff679e
kellyxu1981 <kxu@yahoo-inc.com>
2014-03-22 13:38:06 -0700

    Initial Commit

---
+++ VchatUIFramework/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VchatUIFramework
+//
+//  Created by Kelly Xu on 3/22/14.
+//  Copyright (c) 2014 iosbootcamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VchatUIFrameworkTests/VchatUIFrameworkTests.m
@@ -0,0 +1,34 @@
+//
+//  VchatUIFrameworkTests.m
+//  VchatUIFrameworkTests
+//
+//  Created by Kelly Xu on 3/22/14.
+//  Copyright (c) 2014 iosbootcamp. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VchatUIFrameworkTests : XCTestCase
+
+@end
+
+@implementation VchatUIFrameworkTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
